26fe3378c1a648ca7cdbc9ddb035fc6407506177
SidGames5 <saytleader@outlook.com>
2025-03-17 20:08:50 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+    return <main className="flex w-screen h-screen bg-gray-800">
+        <div className="w-1/5 h-full border-white border-r-2">
+            {/* Sidebar */}
+        </div>
+        <div className="w-1/5 h-full border-white border-r-2">
+            {/* Tasks */}
+        </div>
+        <div className="w-3/5 h-full">
+            {/* Calendar */}
+        </div>
+    </main>;
+}
